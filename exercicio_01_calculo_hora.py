f38475a154bac3e43a064e2f771251cf8b1bcc95
Leonardo <leonardomartinslm9090@gmail.com>
2026-05-21 20:13:51 -0300

    feat: adiciona estrutura basica do exercicio 01

---
+++ exercicio_01_calculo_hora.py
@@ -0,0 +1,7 @@
+salario_hora = float(input("Informe o seu salário por hora: "))
+horas_mes = float(input("Informe a quantidade de horas que você trabalhou no mês: "))
+filhos_menos_14 = int(input("Informe quantos filhos menores de 14 anos que você tem: "))
+
+salario_bruto = 0
+salario_familia = 0
+salario_liquido = 0
