7fff623a85b2ee64b58cf0d86077c94377d4dd73
Leonardo Martins <128648497+Ieozin@users.noreply.github.com>
2026-05-22 15:40:44 +0000

    feat: adiciona calculo de salario liquido, e print para exibir resultados

--- exercicio_01_calculo_hora.py
+++ exercicio_01_calculo_hora.py
@@ -15,3 +15,11 @@ elif salario_bruto <= 1100:
 else:
     print("O salário família será de R$ 11,90 por cada filho.")    
     salario_familia = 11.90
+
+salario_familia_final = salario_familia * filhos_menos_14
+salario_liquido = salario_bruto + salario_familia_final
+
+print("--Folha de pagamento--")
+print(f"O salário bruto será de:R${salario_bruto:.2f}")
+print(f"O salario família será de R${salario_familia_final:.2f}")
+print(f"O salário líquido será de R${salario_liquido:.2f}")
