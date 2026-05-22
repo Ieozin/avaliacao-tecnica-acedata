639997ae9d33d7073c983542d9c6545e0f7e60c4
Leonardo Martins <128648497+Ieozin@users.noreply.github.com>
2026-05-22 15:44:44 +0000

    refactor: centraliza salario familia para resultado final

--- exercicio_01_calculo_hora.py
+++ exercicio_01_calculo_hora.py
@@ -5,15 +5,12 @@ filhos_menos_14 = int(input("Informe quantos filhos menores de 14 anos que você
 salario_bruto = salario_hora * horas_mes
 
 if salario_bruto <= 788:
-    print("O salário família será de R$ 30,50 por cada filho.")
     salario_familia = 30.50
 
 elif salario_bruto <= 1100:
-    print("O salário família será de R$ 18,50 por cada filho.")    
     salario_familia = 18.50
 
-else:
-    print("O salário família será de R$ 11,90 por cada filho.")    
+else:   
     salario_familia = 11.90
 
 salario_familia_final = salario_familia * filhos_menos_14
@@ -21,5 +18,6 @@ salario_liquido = salario_bruto + salario_familia_final
 
 print("--Folha de pagamento--")
 print(f"O salário bruto será de:R${salario_bruto:.2f}")
-print(f"O salario família será de R${salario_familia_final:.2f}")
+print(f"O salário família será de R${salario_familia:.2f} por cada filho")
+print(f"O salario família final será de R${salario_familia_final:.2f}")
 print(f"O salário líquido será de R${salario_liquido:.2f}")
