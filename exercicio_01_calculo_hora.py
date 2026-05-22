0f65ad5fe01a2778cdeb93b1040d6c5444811fe9
Leonardo Martins <128648497+Ieozin@users.noreply.github.com>
2026-05-22 15:25:16 +0000

    feat: adiciona calculo de salario bruto, e condicional para salario familia

--- exercicio_01_calculo_hora.py
+++ exercicio_01_calculo_hora.py
@@ -2,6 +2,16 @@ salario_hora = float(input("Informe o seu salário por hora: "))
 horas_mes = float(input("Informe a quantidade de horas que você trabalhou no mês: "))
 filhos_menos_14 = int(input("Informe quantos filhos menores de 14 anos que você tem: "))
 
-salario_bruto = 0
-salario_familia = 0
-salario_liquido = 0
+salario_bruto = salario_hora * horas_mes
+
+if salario_bruto <= 788:
+    print("O salário família será de R$ 30,50 por cada filho.")
+    salario_familia = 30.50
+
+elif salario_bruto <= 1100:
+    print("O salário família será de R$ 18,50 por cada filho.")    
+    salario_familia = 18.50
+
+else:
+    print("O salário família será de R$ 11,90 por cada filho.")    
+    salario_familia = 11.90
