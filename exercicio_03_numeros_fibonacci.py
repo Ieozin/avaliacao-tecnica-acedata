b6f395ca48a79c02d7c2b6f6c5b0d7b11d518270
Leonardo Martins <128648497+Ieozin@users.noreply.github.com>
2026-05-23 13:33:00 +0000

    fix: corrige verificacao se numero esta na sequencia

--- exercicio_03_numeros_fibonacci.py
+++ exercicio_03_numeros_fibonacci.py
@@ -1,11 +1,11 @@
 sequencia_numero = int(input("Olá, quantos números da sequência de Fibonacci você quer?:"))
 print("Estes são os números:")
 
-
 x = 1
 y = 1
 
 fibonnaci = []
+fibonnaci_01 = []
 
 for i in range(sequencia_numero):
     fibonnaci.append(x)
@@ -15,7 +15,18 @@ for i in range(sequencia_numero):
 
 print(fibonnaci)
 
-if sequencia_numero in fibonnaci:
-    print(f"O número {sequencia_numero} que você escolheu está na sequência de Fibonacci!")
+verificar_numero = int(input("Qual número você quer verificar se está na sequência?:"))
+
+x = 1
+y = 1
+
+while x <= verificar_numero:
+    fibonnaci_01.append(x)
+    z = x
+    x = y 
+    y = z + y
+
+if verificar_numero in fibonnaci_01:
+    print(f"O número {verificar_numero} que você escolheu está na sequência de Fibonacci!")
 else:
-    print(f"O número {sequencia_numero} que você escolheu não está na sequência de Fibonacci!")    
+    print(f"O número {verificar_numero} que você escolheu não está na sequência de Fibonacci!")    
