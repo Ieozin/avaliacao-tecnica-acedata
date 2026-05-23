7e99837c81eca9e4b6608a314790524a13a8dcbf
Leonardo Martins <128648497+Ieozin@users.noreply.github.com>
2026-05-23 14:07:18 +0000

    refactor: adiciona comentarios e organiza codigo

--- exercicio_02_sequencias_numeros.py
+++ exercicio_02_sequencias_numeros.py
@@ -1,11 +1,15 @@
-numeros = []
-
+# Entradas do usuário
 numeros_sequencia = int(input("Quantos números você vai digitar?:"))
 
+# Cria lista vazia
+numeros = []
+
+# Recebe os números e armazena na lista
 for i in range(numeros_sequencia):
     n = float(input("Digite um número: "))
     numeros.append(n)
 
+# Exibe resultado
 print(f"A sequência de números é {numeros}")
 print(f"O maior número da sequência é {max(numeros)}")
 print(f"O menor número da sequência é {min(numeros)}")
