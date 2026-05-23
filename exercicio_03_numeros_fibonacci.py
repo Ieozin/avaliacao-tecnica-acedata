ac8717513cabd02ad9d3ee4bf4812b426448e96a
Leonardo Martins <128648497+Ieozin@users.noreply.github.com>
2026-05-23 12:25:26 +0000

    feat: adiciona verificador se N está na sequencia de Fibonacci

--- exercicio_03_numeros_fibonacci.py
+++ exercicio_03_numeros_fibonacci.py
@@ -1,6 +1,7 @@
 sequencia_numero = int(input("Olá, quantos números da sequência de Fibonacci você quer?:"))
 print("Estes são os números:")
 
+
 x = 1
 y = 1
 
@@ -13,3 +14,8 @@ for i in range(sequencia_numero):
     y = z + y
 
 print(fibonnaci)
+
+if sequencia_numero in fibonnaci:
+    print(f"O número {sequencia_numero} que você escolheu está na sequência de Fibonacci!")
+else:
+    print(f"O número {sequencia_numero} que você escolheu não está na sequência de Fibonacci!")    
