2d7711a78b19c63c767bdfa7ea214ab01f9b4d2d
Leonardo Martins <128648497+Ieozin@users.noreply.github.com>
2026-05-23 11:59:38 +0000

    feat: adiciona estrutura inicial para exercicio 03

---
+++ exercicio_03_numeros_fibonacci.py
@@ -0,0 +1,24 @@
+sequencia_numero = int(print("Olá, quantos números da sequência de Fibonacci você quer?:"))
+print("Estes são os números:")
+
+x = 0
+y = 1
+
+for i in range(sequencia_numero):
+    print(x)
+    x = x + y
+    y = x
+
+
+
+
+
+
+
+
+
+
+
+#Escreva um programa que carregue um valor N pelo teclado e:
+#a) Imprima os N primeiros números da sequência de Fibonacci;
+#b) Indique se N faz parte da sequência de Fibonacci.
