cec03920c4483534348286c23027491b6c60d6b3
Leonardo Martins <128648497+Ieozin@users.noreply.github.com>
2026-05-23 14:18:09 +0000

    refactor: adiciona comentario para exercicio 03

--- exercicio_03_numeros_fibonacci.py
+++ exercicio_03_numeros_fibonacci.py
@@ -1,31 +1,39 @@
+# Entradas do usuário 
 sequencia_numero = int(input("Olá, quantos números da sequência de Fibonacci você quer?:"))
-print("Estes são os números:")
 
+# Valores iniciais da sequência
 x = 1
 y = 1
 
+# Listas para armazenar sequência
 fibonnaci = []
 fibonnaci_01 = []
 
+# Gera sequência pela entrada
 for i in range(sequencia_numero):
     fibonnaci.append(x)
     z = x
     x = y 
     y = z + y
 
-print(fibonnaci)
+# Exibe resultado 
+print(f"Estes são os números: {fibonnaci}")
 
+# Entradas para verificação 
 verificar_numero = int(input("Qual número você quer verificar se está na sequência?:"))
 
+# Reinicia valor
 x = 1
 y = 1
 
+# Gera sequência para verificar
 while x <= verificar_numero:
     fibonnaci_01.append(x)
     z = x
     x = y 
     y = z + y
 
+# Exibe resultado 
 if verificar_numero in fibonnaci_01:
     print(f"O número {verificar_numero} que você escolheu está na sequência de Fibonacci!")
 else:
